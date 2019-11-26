df249c958fcb40cf48697d2dcd555cfb55ef15d0
ptamayo <ptamayo97@yahoo.com>
2019-11-26 11:27:21 -0800

    possible fix

---
+++ src/templates/outreach-page.js
@@ -0,0 +1,77 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
+import "../components/style.css";
+import Layout from "../components/Layout";
+import HeroSection from "../components/HeroSection";
+import Main from "../components/Main";
+import RecentEvents from "../components/RecentEvents";
+import Tabs from "../components/Tabs";
+
+export const OutreachPageTemplate = ({ image, title, subheading }) => (
+  <Fragment>
+    <HeroSection image={image} title={title} subheading={subheading} />
+    <Tabs />
+    <Main>
+      <RecentEvents />
+    </Main>
+  </Fragment>
+);
+
+OutreachPageTemplate.propTypes = {
+  image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+  title: PropTypes.string,
+  heading: PropTypes.string,
+  subheading: PropTypes.string,
+  mainpitch: PropTypes.object,
+  description: PropTypes.string,
+  intro: PropTypes.shape({
+    blurbs: PropTypes.array
+  })
+};
+
+const OutreachPage = ({ data }) => {
+  const { frontmatter } = data.markdownRemark;
+
+  return (
+    <Layout>
+      <OutreachPageTemplate
+        image={frontmatter.image}
+        title={frontmatter.title}
+        heading={frontmatter.heading}
+        subheading={frontmatter.subheading}
+        mainpitch={frontmatter.mainpitch}
+        description={frontmatter.description}
+        intro={frontmatter.intro}
+      />
+    </Layout>
+  );
+};
+
+OutreachPage.propTypes = {
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object
+    })
+  })
+};
+
+export default OutreachPage;
+
+export const pageQuery = graphql`
+  query OutreachPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "outreach-page" } }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 2048, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        subheading
+      }
+    }
+  }
+`;
