6fe28ce56b4df1532af9e7f25dada9493cda6d49
ptamayo <ptamayo97@yahoo.com>
2019-12-06 08:00:17 -0800

    added cms previews

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,6 @@ import uploadcare from "netlify-cms-media-library-uploadcare";
 import cloudinary from "netlify-cms-media-library-cloudinary";
 
 import AboutPagePreview from "./preview-templates/AboutPagePreview";
-// import ProductPagePreview from "./preview-templates/ProductPagePreview";
 import IndexPagePreview from "./preview-templates/IndexPagePreview";
 import CalendarPagePreview from "./preview-templates/CalendarPagePreview";
 import EnvisionPagePreview from "./preview-templates/EnvisionPagePreview";
@@ -11,6 +10,8 @@ import SocialPagePreview from "./preview-templates/SocialPagePreview";
 import ProfessionalPagePreview from "./preview-templates/ProfessionalPagePreview";
 import OutreachPagePreview from "./preview-templates/OutreachPagePreview";
 import ContactPagePreview from "./preview-templates/ContactPagePreview";
+import EventsCompPreview from "./preview-templates/EventsCompPreview";
+import SponsorsCompPreview from "./preview-templates/SponsorsCompPreview";
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
@@ -23,4 +24,5 @@ CMS.registerPreviewTemplate("social", SocialPagePreview);
 CMS.registerPreviewTemplate("professional", ProfessionalPagePreview);
 CMS.registerPreviewTemplate("outreach", OutreachPagePreview);
 CMS.registerPreviewTemplate("contact", ContactPagePreview);
-// CMS.registerPreviewTemplate("products", ProductPagePreview);
+CMS.registerPreviewTemplate("recent-events", EventsCompPreview);
+CMS.registerPreviewTemplate("sponsors", SponsorsCompPreview);
---
+++ src/cms/preview-templates/SponsorsCompPreview.js
@@ -0,0 +1,35 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+import moment from "moment";
+import "../../components/style.css";
+
+const EventsCompPreview = ({ entry, getAsset }) => {
+  const data = entry.getIn(["data"]).toJS();
+
+  if (data) {
+    return (
+      <div className=" container">
+        <div className="row">
+          {data.featuredSponsors.map((sponsor, idx) => (
+            <div className="col-md-4" key={idx}>
+              <figure>
+                <img src={sponsor.sponsorImage} alt={sponsor.title} />
+              </figure>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  } else {
+    return <div>Loading...</div>;
+  }
+};
+
+EventsCompPreview.propTypes = {
+  entry: PropTypes.shape({
+    getIn: PropTypes.func
+  }),
+  getAsset: PropTypes.func
+};
+
+export default EventsCompPreview;
